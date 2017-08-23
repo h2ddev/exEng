3cfecd4aa55b8ab474e00c5fa4c1a616273f8918
Dang Dinh Hop <hop.dd@zinza.com.vn>
2017-08-24 01:04:09 +0700

    popup notify

---
+++ Assets/EnglishBook/Scripts/ManagerTimeNotify.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerTimeNotify : MonoBehaviour
+{ 
+	public GameObject Notify;
+	public static ManagerTimeNotify Instance {
+		get;
+		private set;
+	} 
+	void Awake(){
+		Instance = this;
+	}
+}
---
+++ Assets/EnglishBook/Scripts/TimeCountNotify.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCountNotify : MonoBehaviour
+{ 
+	public float time = 1800f;
+
+	public static TimeCountNotify Instance {
+		get;
+		private set;
+	}
+
+	void Awake ()
+	{
+
+		DontDestroyOnLoad (gameObject);
+		StartCoroutine (CountdownTime ()); 
+	}
+
+	public IEnumerator CountdownTime ()
+	{
+		while (true) {
+			yield return null;
+			time -= Time.deltaTime;  
+			if (time <= 0) {
+				ManagerTimeNotify.Instance.Notify.SetActive (true); 
+				yield return new WaitForSeconds (10f);
+				ManagerTimeNotify.Instance.Notify.SetActive (false);
+				time = 1800f;
+			}
+		}
+	}
+}
